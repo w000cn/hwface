e9e1cf2da59a2abaea8e4965232edb40bc6ef968
w000cn <w000.cn@gmail.com>
2012-07-23 23:32:09 +0800

    网络榜单功能

--- hwface.user.js
+++ hwface.user.js
@@ -88,6 +88,7 @@ function withjQuery(callback, safe){
 withjQuery(function($)
 {
 	var vote_url = "http://hwface.sinaapp.com/vote.php";	
+	var sort_url = "http://hwface.sinaapp.com/sort.php";
 	var giveFiveStr = "<span class=\"fires_icon\" cent=1 lock=0 title=\"一般一般\" style=\"opacity: 0.4; \">&nbsp;</span>" +
 					"<span class=\"fires_icon\" cent=2 lock=0 title=\"可以可以\" style=\"opacity: 0.4; \">&nbsp;</span>" +
 					"<span class=\"fires_icon\" cent=3 lock=0 title=\"不错不错\" style=\"opacity: 0.4; \">&nbsp;</span>" +
@@ -294,17 +295,27 @@ withjQuery(function($)
 		obj.find("span.fires_icon[lock=0]").fadeTo("fast",0.4);
 	}
 	
-	function showSortTable(obj)
+	function showSortTable(obj, network_flag)
 	{
 		$("table.ta_list").find("tr[sort]").remove();
 		var table_obj = $("table.ta_list").find("tr").first();
 		var i = 0;
 		var j = 0;
-		for (i = 5, j = 0; i >= 1; i--)
+		for (i = 50, j = 0; i >= 1; i--)
 		{
 			for (uid in obj)
 			{
-				if (obj[uid] != i){continue;}
+				if (network_flag)
+				{
+					cent = obj[uid]["avg"];
+					count = obj[uid]["count"];
+				}
+				else
+				{
+					cent = obj[uid];
+					if (i %10){continue;}
+				}
+				if (cent*10 != i){continue;}
 				var tr_temp = table_obj.clone();
 				$(tr_temp).attr("class", (j%2)?"list_bm":"list_bm no_bg");
 				var tempa = $(tr_temp).find("td.del span.pr20 a[ajax]");
@@ -315,31 +326,64 @@ withjQuery(function($)
 				temp_str = giveFiveStr.replace(/#url#/, temp_url);
 				tempa.append(temp_str);
 				
-				showLastResult($(tr_temp).find("td.del a[ajax] span.fires_icon").first(), obj[uid]);
+				if (network_flag && hwface[uid])
+				{
+					showLastResult($(tr_temp).find("td.del a[ajax] span.fires_icon").first(), hwface[uid]);
+				}
+				else if (!network_flag)
+				{
+					showLastResult($(tr_temp).find("td.del a[ajax] span.fires_icon").first(), cent);
+				}
 				$(tr_temp).find("td.del").click(function(){objToggle($(this))});
+				$(tr_temp).find("td.del").mouseout(function(){clearFiv($(this))});
+				$(tr_temp).find("span.fires_icon").mouseover(function(){giveFive($(this), 0);});
+				$(tr_temp).find("span.fires_icon").click(function(){giveFive($(this), 1);});
+				
 				$(tr_temp).find("td[align='center'][style]").html("Top "+ (j+1));
 				$(tr_temp).find("td.del_name").html("hwface");
-				$(tr_temp).find("td[align='right'][style]").html("");
-				$(tr_temp).attr("sort", 1);
+				$(tr_temp).find("td[align='right'][style]").html("心动指数:"+cent);
+				$(tr_temp).attr("sort", (network_flag)?"net":"local");
 				table_obj.before(tr_temp);
 				j++;
 			}
 		}
 	}
 	
+	function sort_msg(msg)
+	{
+		$("div#sort_msg").html(msg);
+		$("div#sort_msg").show();
+		$("div#sort_msg").fadeOut(3000);
+	}
+	
 	function local_sort(obj)
 	{
-		//$("div#sort_msg").html("本地排行功能暂时未开放");
-		//$("div#sort_msg").show();
-		//$("div#sort_msg").fadeOut(3000);
+		//sort_msg("本地排行功能暂时未开放");
+		if ($("table.ta_list").find("tr[sort]") && $("table.ta_list").find("tr[sort]").attr("sort") == "local")
+		{return;}
 		showSortTable(hwface);
 	}
 	
 	function net_sort(obj)
 	{
-		$("div#sort_msg").html("网络榜单功能暂时未开放");
-		$("div#sort_msg").show();
-		$("div#sort_msg").fadeOut(3000);
+		//sort_msg("网络榜单功能暂时未开放");	
+		if ($("table.ta_list").find("tr[sort]") && $("table.ta_list").find("tr[sort]").attr("sort") == "net")
+		{return;}
+		$.ajax({
+			url: sort_url,
+			type: "POST",
+			dataType:"jsonp",
+			jsonp:"callback",
+			jsonpCallback:"success_jsonpCallback",
+			success: function(msg)
+			{
+				showSortTable(msg, 1);
+			},
+			error: function(msg)
+			{
+				sort_msg("暂时无法连接投票服务器");
+			}
+		});
 	}
 	
 	$("span.pr20 a").each(function(){getPic($(this));});
